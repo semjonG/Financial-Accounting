f9dc843bf7148c18e0ea4ba4c1a79726eac28353
semjonG <semjon.gerasimov@gmail.com>
2022-01-01 02:10:00 +0300

    Initial Commit

---
+++ Financial accounting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Financial accounting
+//
+//  Created by mac on 01.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
